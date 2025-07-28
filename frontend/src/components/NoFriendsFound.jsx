1287467acf236d6301553c12f638fde6c0509270
Apratim23 <apratimdutta786687@gmail.com>
2025-07-29 02:38:55 +0530

    HomePage Done

---
+++ frontend/src/components/NoFriendsFound.jsx
@@ -0,0 +1,12 @@
+const NoFriendsFound = () => {
+    return (
+        <div className="card bg-base-200 p-6 text-center">
+        <h3 className="font-semibold text-lg mb-2">No Friends Yet</h3>
+        <p className="text-base-content opacity-70">
+          It seems like you don't have any friends added yet.
+        </p>
+        </div>
+    );
+};
+
+export default NoFriendsFound;
