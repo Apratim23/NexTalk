e93af86211b9603a1a7ed13b011cfc2e73d1322f
Apratim23 <apratimdutta786687@gmail.com>
2025-07-29 12:47:05 +0530

    ChatPage Done

--- frontend/src/Pages/ChatPage.jsx
+++ frontend/src/Pages/ChatPage.jsx
@@ -1,11 +1,92 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router';
+import useAuthUser from '../hooks/useAuthUser';
+import { getStreamToken } from '../lib/api';
+import { useQuery } from '@tanstack/react-query';
+
+import {
+  Channel,
+  Chat,
+  ChannelHeader,
+  MessageInput,
+  MessageList,
+  Thread,
+  Window,
+} from 'stream-chat-react';
+import { StreamChat } from 'stream-chat';
+import toast from 'react-hot-toast';
+
+import ChatLoader from '../components/ChatLoader.jsx';
+
+const STREAM_API_KEY = import.meta.env.VITE_STREAM_API_KEY;
 
 const ChatPage = () => {
-  return (
-    <div>
-      ChatPage
+  const { id:targetUserId } = useParams(); // Assuming you want to use the chat ID from the URL
+ const [chatClient, setChatClient] = useState(null);
+ const [channel, setChannel] = useState(null);
+ const [loading, setLoading] = useState(null);
+
+ const {authUser} = useAuthUser();
+
+ const {data:tokenData} = useQuery({
+  queryKey:["streamToken"],
+  queryFn: getStreamToken,
+  enabled: !!authUser
+ });
+
+ useEffect(() => {
+const initChat = async () => {
+if(!tokenData?.token || !authUser) return;
+
+try {
+  console.log("Initializing stream chat client...");
+  const client = StreamChat.getInstance(STREAM_API_KEY);
+
+  await client.connectUser({
+    id: authUser._id,
+    name: authUser.fullName,
+    image: authUser.profilePicture,
+  }, tokenData.token)
+
+  const channelId = [authUser._id, targetUserId].sort().join('-');
+
+  const currChannel = client.channel('messaging', channelId, {
+    members: [authUser._id, targetUserId],
+  });
+
+  await currChannel.watch();
+
+  setChatClient(client);
+  setChannel(currChannel);
+
+} catch (error) {
+  console.error("Error initializing chat client:", error);
+  toast.error("Could not connect to chat. Please try again later.");
+}
+finally{
+  setLoading(false);
+}
+}
+initChat();
+ },[tokenData, authUser, targetUserId]);
+
+ if(loading || !chatClient || !channel) return <ChatLoader />
+
+  return <div className='h-[93vh]'>
+    <Chat client={chatClient}>
+    <Channel channel ={channel}>
+      <div className='w-full relative'>
+        <Window>
+          <ChannelHeader />
+          <MessageList />
+          <MessageInput focus />
+        </Window>
+      </div>
+      <Thread />
+    </Channel>
+    </Chat>
     </div>
-  )
+
 }
 
-export default ChatPage
+export default ChatPage;
