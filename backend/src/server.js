539e656c1c5726ed8363ba48508b3dba31c01d38
Apratim23 <apratimdutta786687@gmail.com>
2025-07-29 14:03:09 +0530

    Changes made - Refactoring

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 import cors from "cors";
@@ -12,6 +13,8 @@ import { connectDB } from "./lib/db.js";
 const app = express();
 const port = process.env.PORT;
 
+const __dirname = path.resolve();
+
 app.use(cors({
     origin:"http://localhost:5173",
     credentials: true,
@@ -22,6 +25,14 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    })
+}
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
